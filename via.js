49471ce891559e38ae50b0964ac3dc183e7a829c
Abhishek Dutta <thelinuxmaniac@gmail.com>
2017-02-07 17:28:44 +0000

    fixed zoom shortcut for Chrome

--- via.js
+++ via.js
@@ -2822,19 +2822,21 @@ window.addEventListener("keydown", function(e) {
     }
 
     // zoom
-    if (e.shiftKey && _via_current_image_loaded) {
-        if (e.which === 61) { // + for zoom in
-            zoom_in();
+    if (_via_current_image_loaded) {
+        // see: http://www.javascripter.net/faq/keycodes.htm
+        if (e.which === 61 || e.which === 187) { // + for zoom in
+            if (e.shiftKey) {
+                zoom_in();
+            } else {  // = for zoom reset
+                reset_zoom_level();
+            }
             return;
         }
-    }
-    if (e.which === 173 && _via_current_image_loaded) { // - for zoom out
-        zoom_out();
-        return;
-    }
-    if (e.which === 61 && _via_current_image_loaded) { // = for zoom reset
-        reset_zoom_level();
-        return;
+
+        if (e.which === 173 || e.which === 189) { // - for zoom out
+            zoom_out();
+            return;
+        }
     }
 
     if ( e.which === 27 ) { // Esc
