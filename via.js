da90a143b06d5243e07b3fad130f6130a4b8df53
Abhishek Dutta <thelinuxmaniac@gmail.com>
2017-10-17 13:34:36 +0100

    via-1.0.4 release

--- via.js
+++ via.js
@@ -50,7 +50,7 @@
 
 */
 
-var VIA_VERSION      = '1.0.3';
+var VIA_VERSION      = '1.0.4';
 var VIA_NAME         = 'VGG Image Annotator';
 var VIA_SHORT_NAME   = 'VIA';
 var VIA_REGION_SHAPE = { RECT:'rect',
