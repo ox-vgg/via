102bb1cf0362d2ebf53d8712c037772d945f6d20
Abhishek Dutta <thelinuxmaniac@gmail.com>
2019-03-26 11:25:21 +0000

    removed stale couchdb store, updated scripts to create couchdb database containing via project

--- scripts/speaker_diarisation/voxceleb/del_couchdb_via_project.py
+++ scripts/speaker_diarisation/voxceleb/del_couchdb_via_project.py
@@ -17,6 +17,7 @@ csv_header = 'project_filename,project_id,set,file_count'
 
 conn = http.client.HTTPConnection(COUCHDB_IP, COUCHDB_PORT)
 
+'''
 index_fn = os.path.join(VIA_FOLDER, 'index.csv');
 with open(index_fn, 'r') as f:
   all_lines_str = f.read()
@@ -33,9 +34,10 @@ with open(index_fn, 'r') as f:
       r = conn.getresponse()
       r.read()
       print('Deleting couchdb database : %d %s' % (r.status, r.reason) )
+'''
 
 # obtained from http://127.0.0.1:5984/_all_dbs
-VIA_PROJECT_LIST = [];
+VIA_PROJECT_LIST = ["v1f9f7e66fd0d482a8ae7a38468cd308d","v1ff7175481f942389fcccef82857f9c6","v2c56bb12f48849669e9dce75c9f5929c","v3e79c313d04744eebc5203f9b7f293ec","v42c179d206e042ebbfbb02a6248f9461","v6f75428e279e4a75bca6374718670779","v81c738031f78436da4ef6e67341122f2","v9013cba11e2c40539fc4676c9c65764c","v9036d02214ec439c9eeff6e27ed20dcc","v9f9243bbe3434391a7d6583ff18475b6","vbd28c6ff99d540a9bc07a81dfbf7c3d0","vc33a5ad24b514c7fa943a023572b1aa3","vd9d5f56857c44b52afaf3df3d717ee3a","ve4b6c03af2f9428fa02040f85a8e58cc"];
 
 for project_id in VIA_PROJECT_LIST:
   uri = '/' + project_id
--- scripts/speaker_diarisation/voxceleb/export_via_project.py
+++ scripts/speaker_diarisation/voxceleb/export_via_project.py
@@ -10,14 +10,25 @@ import uuid
 import http.client
 import datetime
 
-VIA_FOLDER = '/data/datasets/arsha/diarisation/voxceleb/via_projects'
+#VIA_FOLDER = '/data/datasets/arsha/diarisation/voxceleb/via_projects'
+#metadata_fn = '/data/datasets/arsha/diarisation/voxceleb/sel_metadata.pkl'
 
-metadata_fn = '/data/datasets/arsha/diarisation/voxceleb/sel_metadata.pkl'
+COUCHDB_IP = '127.0.0.1'
+COUCHDB_PORT = '5984'
+VIA_FOLDER = '/ssd/adutta/data/via/data/voxceleb/val/via_projects'
+metadata_fn = '/ssd/adutta/data/via/data/voxceleb/val/sel_metadata.pkl'
 metadata = {}
 
 def init_via_project(project_index):
   d = { 'metadata_store':{}, 'file_store':{}, 'file_mid_store':{}, 'attribute_store':{} }
-  d['project_store'] = { 'project_id': str(project_index), 'project_name': ('voxceleb_val_%.3d' % (project_index)), 'data_format_version':'3.0.0', 'creator': 'VGG Image Annotator (http://www.robots.ox.ac.uk/~vgg/software/via)', 'created':datetime.datetime.utcnow().__str__(), 'updated':datetime.datetime.utcnow().__str__() }
+  d['project_store'] = { 
+    'project_id': ('voxceleb_val_693_%.3d' % (project_index)), 
+    'project_name': ('%.3d' % (project_index)), 
+    'data_format_version':'3.0.0', 
+    'creator': 'VGG Image Annotator (http://www.robots.ox.ac.uk/~vgg/software/via)', 
+    'created':datetime.datetime.utcnow().__str__(), 
+    'update_history': [],
+  }
   d['attribute_store']['0'] = { 'id':'0', 'aname':'speaker', 'type':1 }
   d['aid_list'] = ['0'];
   d['fid_list'] = [];
@@ -27,16 +38,33 @@ def save_via_project(json_data, filename):
   with open(filename, 'w') as f:
     json.dump( json_data, f, indent=None, separators=(',',':') )
 
+def push_via_project_to_couchdb(data):
+  uri = baseuri + '/' + data['project_store']['project_id']
+  payload = json.dumps(data)
+  conn.request('PUT', uri, body=payload)
+  r = conn.getresponse()
+  r.read()
+  print('  %s : %d %s' % (uri, r.status, r.reason))
+
 with open(metadata_fn, 'rb') as f:
   all_metadata = pickle.load(f)
 
-filename_list = list(all_metadata.keys())
+## create a new database for the via project
+conn = http.client.HTTPConnection(COUCHDB_IP, COUCHDB_PORT)
+baseuri = '/voxceleb_val_693'
+conn.request('PUT', baseuri )
+r = conn.getresponse()
+r.read()
+print('Create couchdb database : %d %s' % (r.status, r.reason) )
 
-fid = 1
+project_index = 1;
+filename_list = list(all_metadata.keys())
+filename_list.sort();
 
 # metadata[filename] = { 'speaker':speaker, 't0':t0, 't1':t1 }
-for filename in all_metadata:
-  d = init_via_project(fid)
+for filename in filename_list:
+  fid = filename
+  d = init_via_project(project_index)
 
   d['file_store'][fid] = { 'fid':fid, 'filename':filename, 'type':2, 'loc':2, 'src':'http://zeus.robots.ox.ac.uk/via/data/voxceleb/val/avi/' + filename }
   d['fid_list'].append(fid)
@@ -51,9 +79,9 @@ for filename in all_metadata:
     d['file_mid_store'][fid].append(mid)
     mid = mid + 1
 
-  via_project_filename = os.path.join(VIA_FOLDER, ('voxceleb_val_%.3d.json' % (fid)))
+  via_project_filename = os.path.join(VIA_FOLDER, ('voxceleb_val_693_%.3d.json' % (project_index)))
+  print( 'Writing %s : %s' % (filename, via_project_filename) )
   save_via_project(d, via_project_filename)
-  print( 'Writing %s' % (via_project_filename) )
-
-  fid = fid + 1
+  push_via_project_to_couchdb(d)
+  project_index = project_index + 1
 
