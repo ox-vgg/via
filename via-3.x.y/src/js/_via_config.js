438143333fb92954556a41bf186815704f9ad2d1
Abhishek Dutta <thelinuxmaniac@gmail.com>
2021-03-04 15:56:34 +0000

    added https:// url for VIA shared project server

--- via-3.x.y/src/js/_via_config.js
+++ via-3.x.y/src/js/_via_config.js
@@ -2,12 +2,18 @@
 
 const _VIA_NAME = 'VGG Image Annotator';
 const _VIA_NAME_SHORT = 'VIA';
-const _VIA_VERSION = '3.0.9';
+const _VIA_VERSION = '3.0.10';
 const _VIA_URL_CODE_REPO = 'https://gitlab.com/vgg/via';
-const _VIA_URL_PROJECT_PAGE = 'http://www.robots.ox.ac.uk/~vgg/software/via/';
+const _VIA_URL_PROJECT_PAGE = 'https://www.robots.ox.ac.uk/~vgg/software/via/';
 
 const _VIA_CONFIG = { MSG_TIMEOUT:3000 };
-const _VIA_REMOTE_STORE = 'http://zeus.robots.ox.ac.uk/via/store/3.x.y/'; // to host your own server, see https://gitlab.com/vgg/vps
+
+// VIA Project Server (VPS)
+// Use the https://zeus.robots.ox.ac.uk/via/store/3.x.y/ by default
+// For older versions, the http://zeus.robots.ox.ac.uk/via/store/3.x.y/ remains available
+// to host your own server, see https://gitlab.com/vgg/vps
+const _VIA_REMOTE_STORE = 'https://zeus.robots.ox.ac.uk/via/store/3.x.y/';
+//const _VIA_REMOTE_STORE = 'http://zeus.robots.ox.ac.uk/via/store/3.x.y/'; // use if http:// only access is required
 const _VIA_REMOTE_TIMEOUT = 6000; // in milliseconds
 //const _VIA_REMOTE_STORE = 'http://localhost:9669/'; // for debug
 
