8fe2c0e34602563ad6dc848a9c9a6c50d8d24503
IMG-PRCSNG <samthedevil.sp@gmail.com>
2021-03-18 11:59:33 +0000

    add helpful hints to user

--- via-3.x.y/src/js/_via_file_annotator.js
+++ via-3.x.y/src/js/_via_file_annotator.js
@@ -548,6 +548,7 @@ _via_file_annotator.prototype._file_html_element_ready = function() {
   this._creg_draw_all();
 
   this._state_set(_VIA_RINPUT_STATE.IDLE);
+  _via_util_msg_show('File loaded. For tracking, Press <span class="key">Space</span> to play and pause at point of interest', true);
 }
 
 //
